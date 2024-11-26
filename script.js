4e189196055b3176c83fdba7947110dbf5f57e64
Sebastian Asuncion Montero <desarrollo.espinacas1966@gmail.com>
2024-11-26 20:00:56 +0100

    1 - 26/11/2024

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById('infoButton').addEventListener('click', function() {
-    alert('¡Gracias por visitar nuestro tutorial de programación!');
+// Definition constants
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    //console.log(links.classList);
+    //console.log(links.classList.contains("random"));
+    //console.log(links.classList.contains("links"));
+    if (links.classList.contains("show-links")) {
+        links.classList.remove("show-links");
+    } else {
+        links.classList.add("show-links");
+    }
 });
